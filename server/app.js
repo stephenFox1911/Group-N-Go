590bbd56fee084c61dabdea783478034631ea41c
EC2 Default User <ec2-user@ip-172-31-25-64.ec2.internal>
2014-10-02 18:31:36 +0000

    running server

--- server/app.js
+++ server/app.js
@@ -17,8 +17,8 @@ var bodyParser = require('body-parser');
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-var port = process.env.PORT || 8080; 		// set our port
+app.engine('html', require('ejs').renderFile);
+var port = process.env.PORT || 3000; 		// set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
@@ -62,14 +62,15 @@ router.route('/users/:userId')
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-	res.json({ message: 'Hello World!' });	
+	res.render('index.html',{requestIP: req.ip});
+	//res.json({ message: 'Hello World!' });	
 });
 
 // more routes for our API will happen here
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-app.use('/api', router);
+app.use('/', router);
 
 // START THE SERVER
 // =============================================================================
