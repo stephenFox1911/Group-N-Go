38d0b1adc87fa2e90ac88f6464a20f8c9da5d5a1
mli342 <muzi@gatech.edu>
2014-10-17 20:15:58 -0400

    map view added

--- server/views/app.js
+++ server/views/app.js
@@ -2,15 +2,17 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version',
-  'ui.bootstrap'
+	'ngRoute',
+	'myApp.view1',
+	'myApp.view2',
+	'myApp.version',
+	'ui.bootstrap'
 ]).
+
 config(['$routeProvider', function($routeProvider) {
 	$routeProvider.otherwise({redirectTo: '/view1'});
 }]).
+
 controller('NavCtrl', function ($scope, $modal, $log) {
 
 	$scope.open = function (size) {
--- server/views/view2/view2.js
+++ server/views/view2/view2.js
@@ -1,14 +1,35 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute'])
+angular.module('myApp.view2', [
+	'ngRoute',
+	'google-maps'.ns()
+])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
-    templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
-  });
+	$routeProvider.when('/view2', {
+		templateUrl: 'view2/view2.html',
+		controller: 'View2Ctrl'
+	});
+
 }])
 
-.controller('View2Ctrl', [function() {
+.config(['GoogleMapApiProvider'.ns(), function (GoogleMapApi) {
+    GoogleMapApi.configure({
+        //    key: 'your api key',
+        v: '3.17',
+        libraries: 'weather,geometry,visualization'
+    });
+}])
 
+.controller('View2Ctrl', ['$scope', 'GoogleMapApi'.ns(), '$log', function($scope, GoogleMapApi, $log) {
+	$scope.map = {
+	    center: {
+	        latitude: 45,
+	        longitude: -73
+	    },
+	    zoom: 8
+	};
+    GoogleMapApi.then(function(maps) {
+    	$log.info("google maps is ready");
+    });
 }]);
