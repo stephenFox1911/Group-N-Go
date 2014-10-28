6f0f7c32f23e85fd53adf2449e2bbc68d0d4cd87
mli342 <muzi@gatech.edu>
2014-10-28 12:29:31 -0400

    login/logout/signup api integrated

--- server/views/controllers/login.js
+++ server/views/controllers/login.js
@@ -1,9 +1,31 @@
 'user strict';
-app.controller('LoginCtrl', function($scope, $location){
-	console.log("login");
-	$scope.loginf = function(){
-		$scope.login.isAuth = true;
-		$location.path('/view1');
-	}
+app.controller('LoginCtrl', function($scope, $location, $http, authService){
+
+	$scope.loginData = {
+		uname: "",
+		pass: ""
+	};
+
+	$scope.message = "";
+
+	$scope.login = function () {
+
+        authService.login($scope.loginData).then(function (response) {
+
+            $location.path('/view1');
+
+        },
+        function (err) {
+            $scope.message = err.error_description;
+        });
+    };
+
+	// $scope.loginf = function(){
+	// 	$scope.login.isAuth = true;
+	// 	$location.path('/view1');
+	// }
+
+
+
 
 });
--- server/views/controllers/signup.js
+++ server/views/controllers/signup.js
@@ -1,4 +1,40 @@
 'user strict';
-app.controller('SignupCtrl', function($scope){
+app.controller('SignupCtrl', function($scope, $location, $timeout, authService){
+    $scope.savedSuccessfully = false;
+    $scope.message = "";
 
+    $scope.registration = {
+        uname: "",
+        pass: "",
+        // confirmPassword: "",
+        fname: "",
+        lname: ""
+    };
+
+    $scope.signUp = function () {
+
+        authService.saveRegistration($scope.registration).then(function (response) {
+
+            $scope.savedSuccessfully = true;
+            $scope.message = "User has been registered successfully, you will be redicted to login page in 2 seconds.";
+            startTimer();
+            console.log(response);
+        },
+		function (response) {
+			var errors = [];
+			for (var key in response.data.modelState) {
+				for (var i = 0; i < response.data.modelState[key].length; i++) {
+				errors.push(response.data.modelState[key][i]);
+				}
+			}
+			$scope.message = "Failed to register user due to:" + errors.join(' ');
+		});
+    };
+
+    var startTimer = function () {
+        var timer = $timeout(function () {
+            $timeout.cancel(timer);
+            $location.path('/login');
+        }, 2000);
+    }
 });
